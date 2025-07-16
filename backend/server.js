e9b97d8c60d224b6a87e5e5c9dc239b4768596ea
ronkeadun <ronkeadun1@yahoo.com>
2025-07-16 11:54:33 +0100

    sorting out cors issue after deployment

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const authRoutes = require('./routes/auth');
-const movieRoutes = require('./routes/movies');
-const userRoutes = require('./routes/users');
-const favoriteRoutes = require('./routes/favorites');
-const watchlistRoutes = require('./routes/watchlist');
-const watchedRoutes = require('./routes/watched');
-const ratingReviewsRoutes = require('./routes/ratingReviews')
-const errorHandler = require('./middleware/errorHandler');
+const authRoutes = require("./routes/auth");
+const movieRoutes = require("./routes/movies");
+const userRoutes = require("./routes/users");
+const favoriteRoutes = require("./routes/favorites");
+const watchlistRoutes = require("./routes/watchlist");
+const watchedRoutes = require("./routes/watched");
+const ratingReviewsRoutes = require("./routes/ratingReviews");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
-app.use(cors({origin: process.env.CLIENT_BASE_URL || "http://localhost:5173", credentials: true}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_BASE_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 const port = process.env.PORT || 5000;
 
@@ -29,29 +34,30 @@ app.get("/", (req, res) => {
 });
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/movies', movieRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/favorites', favoriteRoutes);
-app.use('/api/watchlist', watchlistRoutes);
-app.use('/api/watched', watchedRoutes);
-app.use('/api/rating-reviews', ratingReviewsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/movies", movieRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/favorites", favoriteRoutes);
+app.use("/api/watchlist", watchlistRoutes);
+app.use("/api/watched", watchedRoutes);
+app.use("/api/rating-reviews", ratingReviewsRoutes);
 
 // 404 handler for invalid routes
 app.use((req, res, next) => {
-  res.status(404).json({ error: 'Route not found' });
+  res.status(404).json({ error: "Route not found" });
 });
 
 // Centralized error handler
 app.use(errorHandler);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT || 5000, () => {
-      console.log('Server running on port', port);
+      console.log("Server running on port", port);
     });
   })
   .catch((err) => {
     console.log({ err });
     process.exit(1);
-});
+  });
